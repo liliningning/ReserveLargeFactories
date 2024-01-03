4a6764f686554dd2610a461ee3c7c654fea72880
吕子衡 <you@example.com>
2024-01-03 21:23:19 +0800

    [新增]新增通讯录框架

---
+++ addressBook.c
@@ -0,0 +1,33 @@
+#include "addressBook.h"
+/*初始化通讯录*/
+int addressBookInit(AddressBook **addrBook)
+{
+}
+/*添加联系人*/
+int addressBookAddPerson(AddressBook *addrBook, PersonData person)
+{
+}
+/*根据名字删除联系人*/
+int addressBookDeletePerson(AddressBook *addrBook, char *name)
+{
+}
+/*通过名字查找电话号码*/
+int addressBookSeekPhone(AddressBook *addrBook, char *name)
+{
+}
+/*修改某人信息*/
+int addressBookModify(AddressBook *addrBook, char *name)
+{
+}
+/*按照名字给通讯录联系人排序*/
+int addressBookSort(AddressBook *addrBook)
+{
+}
+/*打印通讯录*/
+void addressBookPrint(AddressBook *addrBook)
+{
+}
+/*清空通讯录*/
+int ruinAddressBook(AddressBook *addrBook)
+{
+}
---
+++ main.c
@@ -0,0 +1,5 @@
+#include "addressBook.h"
+int main()
+{
+    return 0;
+}
