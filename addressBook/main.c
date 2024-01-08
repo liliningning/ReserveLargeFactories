afc6f5fe6781b66348a4cd6868907d16870ce9a8
liliningning <2914352881@qq.com>
2024-01-08 16:24:06 +0800

    [新增] 新增通讯录的菜单

--- addressBook/main.c
+++ addressBook/main.c
@@ -1,28 +1,91 @@
 #include "addressBook.h"
 #include <stdio.h>
+
+enum STATUS_CODE
+{
+    ADD = 1,
+    SEEK = 2,
+    MODIFY = 3,
+    DELETE = 4,
+    SAVE = 5,
+
+};
+
+void menu()
+{
+     printf("                       \n");
+    printf("    1:添加联系人         \n");
+    printf("    2:查找联系人         \n");
+    printf("    3:修改联系人         \n");
+    printf("    4:删除联系人         \n");
+    printf("    5:保存联系人         \n");
+    printf("                        \n");
+}
+
+
 int main()
 {
+    /* 初始化 */
     AddressBook *book = NULL;
     addressBookInit(&book);
 
-    printf("add person li\n");
-    PersonData newPerson2 = {"lining", 'w', 18, "10086", "伊犁师范大学"};
-    addressBookAddPerson(book, newPerson2);
+    int choice = 0;
+    printf("请输入你的选项: \n");
+    scanf("%d", &choice);
 
-    printf("add person an\n");
-    PersonData newPerson1 = {"anzhenyu", 'm', 18, "12138", "皖江工学院"};
-    addressBookAddPerson(book, newPerson1);
+    switch(choice)
+    {
+    case ADD:
+    {
+        addressBookAddPerson(&book,);
+        break;
+    }
+    case SEEK:
+    {
+        break;
+    }
+    case MODIFY:
+    {
+        break;
+    }
+    case DELETE:
+    {
+        break;
+    }
+    case SAVE:
+    {
+        break;
+    }
 
-    addressBookSort(book);
+    }
 
-    addressBookPrint(book);
 
-    // printf("delete person an\n");
-    // addressBookDeletePerson(book, "anzhenyu");
+
+
+
+
+
+
+
+
+    // printf("add person li\n");
+    // PersonData newPerson2 = {"lining", 'w', 18, "10086", "伊犁师范大学"};
+    // addressBookAddPerson(book, newPerson2);
+
+    // printf("add person an\n");
+    // PersonData newPerson1 = {"anzhenyu", 'm', 18, "12138", "皖江工学院"};
+    // addressBookAddPerson(book, newPerson1);
+
+    // addressBookSort(book);
+
     // addressBookPrint(book);
 
-    // char *zhenyuPhone = addressBookSeekPhone(book, "anzhenyu");
-    // printf("zhenyuPhone=%s\n", zhenyuPhone);
+    // // printf("delete person an\n");
+    // // addressBookDeletePerson(book, "anzhenyu");
+    // // addressBookPrint(book);
+
+    // // char *zhenyuPhone = addressBookSeekPhone(book, "anzhenyu");
+    // // printf("zhenyuPhone=%s\n", zhenyuPhone);
 
     return 0;
 }
