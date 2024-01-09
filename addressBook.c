b5ffaf220b2ea9d4973c3754c077c3395eec57f4
User7575 <2589391494@qq.com>
2024-01-09 21:12:16 +0800

    [新增]新增清空通讯录操作

---
+++ addressBook.c
@@ -0,0 +1,216 @@
+#include "addressBook.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+enum STATUS_CODE
+{
+    CALLLOC_ERROR = -4,
+    NULL_PTR,
+    INVALID_NAME,
+    INVALID_POS,
+    ON_SUCCESS,
+};
+/*****************************静态函数声明*************************************/
+static int checkBook(AddressBook *addrBook);
+static int addressBookPosAddPerson(AddressBook *addrBook, PersonData person, int pos);
+static int deleteCurrentNode(BookNode *deleteNode);
+/*****************************静态函数实现*************************************/
+static int checkBook(AddressBook *addrBook)
+{
+    if (addrBook == NULL)
+    {
+        return NULL_PTR;
+    }
+}
+static int addressBookPosAddPerson(AddressBook *addrBook, PersonData person, int pos)
+{
+    /*尾插添加联系人，默认在通讯录末尾*/
+    checkBook(addrBook);
+    /*为新节点开辟空间*/
+    BookNode *newNode = calloc(1, sizeof(BookNode));
+    /*判空*/
+    if (newNode == NULL)
+    {
+        return CALLLOC_ERROR;
+    }
+    newNode->person = calloc(1, sizeof(PersonData));
+    /*判空*/
+    if (newNode->person == NULL)
+    {
+        return CALLLOC_ERROR;
+    }
+
+    /*维护新节点*/
+    strncpy(newNode->person->addrs, person.addrs, sizeof(newNode->person->addrs) - 1);
+    // newNode->person->addrs[sizeof(newNode->person->addrs) - 1] = '\0';
+    strncpy(newNode->person->name, person.name, sizeof(newNode->person->name) - 1);
+    // newNode->person->name[sizeof(newNode->person->name) - 1] = '\0';
+    strncpy(newNode->person->phone, person.phone, sizeof(newNode->person->phone) - 1);
+    // newNode->person->phone[sizeof(newNode->person->phone) - 1] = '\0';
+    newNode->person->sex = person.sex;
+    newNode->person->age = person.age;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    if (pos < 0 || pos > addrBook->len)
+    {
+        return INVALID_POS;
+    }
+    BookNode *travelNode = addrBook->head;
+    for (int idx = 0; idx < pos && travelNode != NULL; idx++)
+    {
+        travelNode = travelNode->next;
+    }
+    /*改变节点指针方向*/
+    newNode->next = travelNode->next;
+    newNode->prev = travelNode;
+    travelNode->next = newNode;
+    if (travelNode->next != NULL)
+    {
+        travelNode->next->prev = newNode;
+    }
+
+    /*长度增加*/
+    addrBook->len++;
+    return ON_SUCCESS;
+}
+/*删除当前结点*/
+static int deleteCurrentNode(BookNode *deleteNode)
+{
+    BookNode *preDeleteNode = deleteNode->prev;
+    preDeleteNode->next = deleteNode->next;
+    if (deleteNode->next != NULL)
+    {
+        deleteNode->next->prev = preDeleteNode;
+    }
+
+    if (deleteNode != NULL)
+    {
+        free(deleteNode);
+        deleteNode = NULL;
+    }
+}
+
+/*****************************分割线*************************************/
+/*初始化通讯录*/
+int addressBookInit(AddressBook **addrBook)
+{
+    AddressBook *adBook = calloc(1, sizeof(AddressBook));
+    if (adBook == NULL)
+    {
+        return CALLLOC_ERROR;
+    }
+    adBook->head = calloc(1, sizeof(BookNode));
+    if (adBook->head == NULL)
+    {
+        return CALLLOC_ERROR;
+    }
+    adBook->len = 0;
+    adBook->head->person = NULL;
+    adBook->head->next = NULL;
+    adBook->head->prev = adBook->head;
+    /*解应用*/
+    *addrBook = adBook;
+}
+/*添加联系人*/
+int addressBookAddPerson(AddressBook *addrBook, PersonData person)
+{
+    return addressBookPosAddPerson(addrBook, person, addrBook->len);
+}
+
+/*根据名字删除联系人*/
+int addressBookDeletePerson(AddressBook *addrBook, char *name)
+{
+    checkBook(addrBook);
+    BookNode *travelNode = addrBook->head->next;
+    while (travelNode != NULL && strcmp(travelNode->person->name, name) != 0)
+    {
+        travelNode = travelNode->next;
+    }
+    /*退出循环，找到了||找完了都没有*/
+    if (travelNode == NULL)
+    {
+        return INVALID_NAME;
+    }
+    else
+    {
+        deleteCurrentNode(travelNode);
+    }
+    return ON_SUCCESS;
+}
+/*通过名字查找电话号码*/
+int addressBookSeekPhone(AddressBook *addrBook, char *name)
+{
+
+}
+/*修改某人信息*/
+int addressBookModify(AddressBook *addrBook, char *name)
+{
+
+}
+/*按照名字给通讯录联系人排序*/
+int addressBookSort(AddressBook *addrBook)
+{
+
+}
+/*打印通讯录*/
+void addressBookPrint(AddressBook *addrBook)
+{
+    checkBook(addrBook);
+    BookNode *travelNdoe = addrBook->head->next;
+    if (travelNdoe == NULL)
+    {
+        printf("The address book is empty.\n");
+        return;
+    }
+    while (travelNdoe != NULL)
+    {
+        printf("name=%s\nsex=%c\nage=%d\nphone=%s\naddrs=%s\n", travelNdoe->person->name, travelNdoe->person->sex, travelNdoe->person->age, travelNdoe->person->phone, travelNdoe->person->addrs);
+        travelNdoe = travelNdoe->next;
+        printf("\n");
+    }
+    printf("\n");
+}
+//获取通讯录长度/
+int addressBookGetLength(AddressBook *addrBook, int *size)
+{
+    int ret = 0;
+    if (addrBook == NULL)
+    {
+        return NULL_PTR;
+    }
+
+    if (size)
+    {
+        *size = addrBook->len;
+    }
+    /* 返回通讯录的长度 */
+    return addrBook->len;
+}
+
+
+/*清空通讯录*/
+int ruinAddressBook(AddressBook *addrBook)
+{
+    int ret = 0;
+    int size = 0;
+    BookNode *travelNode=addrBook->head->next;
+    BookNode *temp=NULL;
+    while(addressBookGetLength (addrBook, &size))
+    {
+        temp=travelNode;
+        deleteCurrentNode(temp);
+        travelNode=travelNode->next;
+    }
+    if (addrBook->head != NULL)
+    {
+        free(addrBook->head);
+        addrBook->head = NULL;
+    }
+    if (addrBook!= NULL)
+    {
+        free(addrBook);
+        addrBook=NULL;
+    }
+    
+    return ret;
+}
